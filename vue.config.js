09c1b78de3f2e9b59e2b53fbd23f069449db0319
Christopher Meyer <chris@schwiiz.org>
2021-04-06 10:41:00 +0200

    libs

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,16 @@ const outputDir = "C:/OPENTEXT/support/rhcore/vue-widgets/"
 const webpack = require('webpack')
 
 module.exports = {
-	// outputDir,
+	outputDir,
 	configureWebpack: {
 		plugins: [
 			new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
 		]
+		// output: {
+		// 	libraryExport: 'default',
+		// 	libraryTarget: 'umd'
+		// }
 	},
 	transpileDependencies: ['@kweli/cs-rest']
+
 }
