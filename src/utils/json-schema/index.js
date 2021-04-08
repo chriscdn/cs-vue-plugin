6e575f8dd9210cae239fbf15bc74900bbb6d4773
Christopher Meyer <chris@schwiiz.org>
2021-04-08 14:02:31 +0200

    fixed IE11 issue with Ajv

--- src/utils/json-schema/index.js
+++ src/utils/json-schema/index.js
@@ -1,25 +1,28 @@
 const Ajv = require('ajv')
 const ajv = new Ajv({
-	allErrors: true
+	allErrors: true,
+	code: {
+		es5: true // https://www.npmjs.com/package/ajv/v/7.1.1#using-in-es5-environment
+	}
 })
 const moment = require('moment')
 
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'))
 
 ajv.addFormat('user', {
-    type: 'integer',
+	type: 'integer',
 })
 
 ajv.addFormat('usergroup', {
-    type: 'integer',
+	type: 'integer',
 })
 
 ajv.addFormat('date-time', {
-  validate: dateTimeString => moment.utc(dateTimeString).isValid()
+	validate: dateTimeString => moment.utc(dateTimeString).isValid()
 })
 
 ajv.addFormat('date', {
-  validate: dateTimeString => moment.utc(dateTimeString).isValid()
+	validate: dateTimeString => moment.utc(dateTimeString).isValid()
 })
 
 function validate(schema, data) {
